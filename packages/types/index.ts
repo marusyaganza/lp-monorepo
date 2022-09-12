bfa351b9d44be67fd71590a7c5443cde2ba5180c
Marusya Ganza <maursyaganza@yandex.ru>
2022-09-12 16:43:42 +0200

    chore: add api-extractor

--- packages/types/index.ts
+++ packages/types/index.ts
@@ -1,2 +1,1 @@
-export * from './src/types';
-export * from './src/common-types';
+export * from './src';
--- packages/types/src/common-types.ts
+++ packages/types/src/common-types.ts
@@ -1,19 +1,19 @@
 export type LinkType = {
-    url: string;
-    text: string;
-  };
+  url: string;
+  text: string;
+};
 
-  export type WordType = {
-    id: string,
-    name: string,
-    defs: string[],
-    particle: string,
-    user:string
-    uuid?: string
-    transcription?: string,
-    imgUrl?: string,
-    audioUrl?: string,
-    tags?: string[],
-    additionalInfo?: string,
-    examples?: string[],
-  }
+export type WordType = {
+  id: string;
+  name: string;
+  defs: string[];
+  particle: string;
+  user: string;
+  uuid?: string;
+  transcription?: string;
+  imgUrl?: string;
+  audioUrl?: string;
+  tags?: string[];
+  additionalInfo?: string;
+  examples?: string[];
+};
