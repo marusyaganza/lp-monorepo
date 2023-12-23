117f8fd4404840e23ae283a7fc4993a0a601e1cc
Maria Ganza <mariia_ganza@protonmail.com>
2023-12-23 19:02:41 +0100

    chore: upgrade plop

--- packages/ui/plopfile.js
+++ packages/ui/plopfile.js
@@ -1,26 +1,30 @@
 module.exports = function (plop) {
-    // component generator
-    plop.setGenerator('controller', {
-        description: 'create new component logic',
-        prompts: [{
-            type: 'input',
-            name: 'name',
-            message: 'component name please'
-        }],
-        actions: [{
-            type: 'add',
-            path: 'src/components/{{pascalCase name}}/{{pascalCase name}}.tsx',
-            templateFile: 'plop-templates/Template/Template.hbs'
-        }, 
-        {
-            type: 'add',
-            path: 'src/components/{{pascalCase name}}/{{pascalCase name}}.module.css',
-            templateFile: 'plop-templates/Template/Template.module.css'
-        },
-        {
-            type: 'add',
-            path: 'src/components/{{pascalCase name}}/{{pascalCase name}}.stories.tsx',
-            templateFile: 'plop-templates/Template/Template.stories.hbs'
-        }],
-    });
+  // component generator
+  plop.setGenerator('controller', {
+    description: 'create new component logic',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'component name please'
+      }
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: 'src/components/{{pascalCase name}}/{{pascalCase name}}.tsx',
+        templateFile: 'plop-templates/Template/Template.hbs'
+      },
+      {
+        type: 'add',
+        path: 'src/components/{{pascalCase name}}/{{pascalCase name}}.module.css',
+        templateFile: 'plop-templates/Template/Template.module.css'
+      },
+      {
+        type: 'add',
+        path: 'src/components/{{pascalCase name}}/{{pascalCase name}}.stories.tsx',
+        templateFile: 'plop-templates/Template/Template.stories.hbs'
+      }
+    ]
+  });
 };
