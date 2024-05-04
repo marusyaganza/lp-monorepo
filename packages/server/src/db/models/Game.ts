bb5520f838583b878b3e0cc77b1eedbb590328c3
Maria Ganza <mariia_ganza@protonmail.com>
2024-05-04 20:20:24 +0200

    fix: delete games mock

--- packages/server/src/db/models/Game.ts
+++ packages/server/src/db/models/Game.ts
@@ -1,7 +1,6 @@
 import { GameConfig, Language } from '../../generated/graphql';
 import { formatData, formatFilter } from '../helpers';
 import { Game } from '../schema/Game';
-// import { games as gamesMock } from '../../mocks/games';
 
 export interface GameFilterType extends Partial<GameConfig> {
   languages?: Language;
@@ -20,7 +19,6 @@ export const GameModel: GameModelType = {
 
   async findMany(filter) {
     const games = await Game.find(formatFilter(filter));
-    // await Game.insertMany(gamesMock);
     return games;
   }
 };
