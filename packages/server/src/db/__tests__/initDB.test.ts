9b447b8d4f7ec049f88dab56bd0f01198e773b94
Maria Ganza <mariia_ganza@protonmail.com>
2025-08-03 09:07:32 +0200

    test(server): update unit tests

--- packages/server/src/db/__tests__/initDB.test.ts
+++ packages/server/src/db/__tests__/initDB.test.ts
@@ -14,45 +14,27 @@ jest.mock('mongoose', () => {
 
 describe('initDB', () => {
   const mockConnect = connect as jest.Mock;
-  const mockCb = jest.fn();
 
   beforeEach(() => {
     jest.clearAllMocks();
-  });
-
-  it('should call the callback when connection is successful', async () => {
-    // Simulate successful connection
-    mockConnect.mockResolvedValueOnce({});
-
-    await initDB(mockCb);
-    expect(mockCb).toHaveBeenCalled();
-  });
-
-  it('should not call the callback when connection fails', async () => {
-    const mockError = new Error('Connection failed');
-    mockConnect.mockRejectedValueOnce(mockError);
-    await initDB(mockCb);
-    expect(mockCb).not.toHaveBeenCalled();
   });
 
   it('should use the provided connection string when passed', async () => {
     const customConnectionString = 'mongodb://custom-db:27017';
     mockConnect.mockResolvedValueOnce({});
 
-    await initDB(mockCb, customConnectionString);
+    await initDB(customConnectionString);
 
     expect(mockConnect).toHaveBeenCalledWith(customConnectionString);
-    expect(mockCb).toHaveBeenCalled();
   });
 
   it('should use the default connection string when none is provided', async () => {
     mockConnect.mockResolvedValueOnce({});
 
-    await initDB(mockCb);
+    await initDB();
 
     expect(mockConnect).toHaveBeenCalledWith(
       mockEnv.MONGO_CONNECTION || DEFAULT_MONGO_CONNECTION
     );
-    expect(mockCb).toHaveBeenCalled();
   });
 });
