83af3f0bdb742f572aa994eb2d599b8e197a12d2
Maria Ganza <mariia_ganza@protonmail.com>
2025-08-09 11:03:21 +0200

    fix: set mongodb version to 4.2

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -1,9 +1,9 @@
-import express from 'express';
+import express, { Express } from 'express';
 import cors from 'cors';
 
 const FE_URL = process.env.FE_URL;
 
-const app = express();
+const app: Express = express();
 app.use(express.json());
 app.use(cors({ origin: FE_URL }));
 
