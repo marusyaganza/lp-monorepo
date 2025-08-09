78f78787e56cbaa258fe3efa101316569c0aa768
Maria Ganza <mariia_ganza@protonmail.com>
2025-08-09 14:45:57 +0200

    fix: only enable rate limiting in production

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -3,16 +3,19 @@ import cors from 'cors';
 import rateLimit from 'express-rate-limit';
 
 const FE_URL = process.env.FE_URL;
-
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100,
-  message: 'Too many requests from this IP, please try again later.'
-});
-
+const isProd = process?.env?.NODE_ENV === 'production';
 const app: Express = express();
+
 app.use(express.json());
 app.use(cors({ origin: FE_URL }));
-app.use(limiter);
+
+if (isProd) {
+  const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100,
+    message: 'Too many requests from this IP, please try again later.'
+  });
+  app.use(limiter);
+}
 
 export default app;
