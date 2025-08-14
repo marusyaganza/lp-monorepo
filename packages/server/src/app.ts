d671909ee527f26b2b73576714c3b80543804bb1
Maria Ganza <mariia_ganza@protonmail.com>
2025-08-14 12:00:24 +0200

    chore(server): add ping endpoint

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -3,11 +3,18 @@ import cors from 'cors';
 import rateLimit from 'express-rate-limit';
 
 const FE_URL = process.env.FE_URL;
+const isDemo = process?.env?.DEMO_VERSION === 'true';
 const isProd = process?.env?.NODE_ENV === 'production';
 const app: Express = express();
 
 app.use(express.json());
 app.use(cors({ origin: FE_URL }));
+
+if (isDemo) {
+  app.get('/ping', (_, res) => {
+    res.status(200).send('OK');
+  });
+}
 
 if (isProd) {
   const limiter = rateLimit({
